916a502d8d25a25ad12077e219e450d766f3f5c0
burisano <brycekalden@gmail.com>
2021-03-03 16:14:44 -0800

    Initialized Files

---
+++ WeSplit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeSplit
+//
+//  Created by Bryce on 2021-03-03.
+//  Copyright © 2021 Bryce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
